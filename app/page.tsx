b40691979771c3b880049e71a2ab84aac367922d
Lawitadelimon <lau72abrvjua@gmail.com>
2025-02-24 11:30:06 -0600

    03 Categories on the fron page (static)

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-
+import Categories from "./components/Categories";
+import Cateories from "./components/Categories";
 export default function Home() {
   return (
     <main className="">
-      Django Bnb
-      <h2 className="text-airbnb">Django and next rules</h2>
+      <Categories/>
     </main>
   );
 }
