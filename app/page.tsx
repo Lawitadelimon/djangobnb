026f17d659fe14cb5e403120d75c793e5e71ede2
Lawitadelimon <lau72abrvjua@gmail.com>
2025-02-24 11:58:54 -0600

    04 List properties on the from page (state)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
 import Categories from "./components/Categories";
-import Cateories from "./components/Categories";
+import PropertyList from "./components/properties/PropertyList";
+
 export default function Home() {
   return (
     <main className="">
       <Categories/>
+
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
+        <PropertyList/>
+      </div>
+
+      
     </main>
   );
 }
