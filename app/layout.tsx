4e89a9dc864bc5743144277fc8eab3cebc27f37f
Lawitadelimon <lau72abrvjua@gmail.com>
2025-03-03 23:36:38 -0600

    12 Create reusable modal window

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter} from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modals/Modal";
 
 const inter = Inter({ subsets: ['latin']});
 
@@ -17,6 +18,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const content = (
+    <p>yo yo</p>
+  ) 
+  
   return (
     <html lang="en">
       <body className={ inter.className}>
