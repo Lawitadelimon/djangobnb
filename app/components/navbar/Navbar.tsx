601b4b3bcae8398fd6d5bb6e4ee05a62a84a3082
Lawitadelimon <lau72abrvjua@gmail.com>
2025-03-12 21:32:21 -0600

    18 Log out

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,37 +1,46 @@
 import Image from 'next/image';
-import Link from "next/link";
+import Link from 'next/link';
 
 import SearchFilters from './SearchFilters';
 import UserNav from './UserNav';
+import { getUserId } from '@/app/lib/actions';
 import AddPropertyButton from './AddPropertyButton';
 
+const Navbar = async () => {
+    const userId = await getUserId();
 
+    console.log('userId:', userId);
 
-const Navbar = () => {
     return (
-    <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
-        <div className="max-w-[15000px] mx-auto px-6">
-            <div className="flex justify-between items-center">
-                <Link href="/">
-                    <Image
-                        src="/logo.png"
-                        alt='DjangoBnb logo'
-                        width={180}
-                        height={38}
+        <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
+            <div className="max-w-[1500px] mx-auto px-6">
+                <div className="flex justify-between items-center">
+                    <Link href="/">
+                        <Image
+                            src="/logo.png"
+                            alt="DjangoBnb logo"
+                            width={180}
+                            height={38}
                         />
-                </Link>
+                    </Link>
 
-                <div className='flex space-x-6'>
-                    <SearchFilters/>
-                </div>
-                <div className='flex items-center space-x-6'>
-                    <AddPropertyButton/>
-                    <UserNav/>
+                    <div className="flex space-x-6">
+                        <SearchFilters />
+                    </div>
+
+                    <div className="flex items-center space-x-6">
+                        <AddPropertyButton 
+                            userId={userId}
+                        />
+
+                        <UserNav 
+                            userId={userId}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
-    </nav>
-    ) 
+        </nav>
+    )
 }
 
 export default Navbar;
