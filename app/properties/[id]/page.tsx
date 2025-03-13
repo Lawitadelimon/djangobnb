8a15bd7e710c0aabd711e5b2242cfcaa6561f1e9
Lawitadelimon <lau72abrvjua@gmail.com>
2025-03-13 11:17:31 -0600

    20 detail page for properties

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,50 +1,68 @@
 import Image from "next/image";
+import Link from "next/link";
 import ReservationSidebar from "@/app/components/properties/ReservationSidebar";
 
-const PropertyDetailPage = () => {
-    return(
+import apiService from "@/app/services/apiService";
+import { getUserId } from "@/app/lib/actions";
+
+const PropertyDetailPage = async ({params}: { params: {id: string }}) => {
+    const property = await apiService.get(`/api/properties/${params.id}`);
+    const userId = await getUserId();
+
+    console.log('userId', userId);
+
+    return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
-        <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
-            <Image
-            fill
-            src='/beach_1.jpg'
-            className="object-cover w-full h-full"
-            alt="Beach house"
-            />
-        </div>
-        <div className="my-4 grid grid-cols-1 md:grid-cols-5 gap-4 ">
-            <div className="py-6 pr-6 col-span-3">
-                <h1 className="mb-4 text-4xl ">Property name</h1>
-
-                <span className="mb-6 block text-lg text-gray-600">
-                    4 guests - 2 bedroonms - 1 bathroom
-                </span>
-                <hr />
-
-                <div className="py-6 flex items-center space-x-4">
-                    <Image
-                    src="/profile_pic_1.jpg"
-                    width={50}
-                    height={50}
-                    className="rounded-full"
-                    alt="The user name"
-                    />
-                    <p><strong>Jhon Doe</strong> is your host</p>
-                </div>
-
-                <hr />
-                
-                <p className="mt-6 text-lg"> Lkjjjfceruirfhi 
-                    lefjriofjir lwekfnrekjbfbjrbf lknfcejrfn
-                </p>
-                
+            <div className="w-full h-[64vh] mb-4 overflow-hidden rounded-xl relative">
+                <Image
+                    fill
+                    src={property.image_url}
+                    className="object-cover w-full h-full"
+                    alt="Beach house"
+                />
             </div>
 
-            <ReservationSidebar/>
-        </div>
-    </main>
+            <div className="grid grid-cols-1 md:grid-cols-5 gap-4">
+                <div className="py-6 pr-6 col-span-3">
+                    <h1 className="mb-4 text-4xl">{property.title}</h1>
+
+                    <span className="mb-6 block text-lg text-gray-600">
+                        {property.guests} guests - {property.bedrooms} bedrooms - {property.bathrooms} bathrooms
+                    </span>
+
+                    <hr />
+
+                    <Link 
+                        href={`/landlords/${property.landlord.id}`}
+                        className="py-6 flex items-center space-x-4"
+                    >
+                        {property.landlord.avatar_url && (
+                            <Image
+                                src={property.landlord.avatar_url}
+                                width={50}
+                                height={50}
+                                className="rounded-full"
+                                alt="The user name"
+                            />
+                        )}
+
+                        <p><strong>{property.landlord.name}</strong> is your host</p>
+                    </Link>
+
+                    <hr />
+
+                    <p className="mt-6 text-lg">
+                        {property.description}
+                    </p>
+                </div>
+
+                <ReservationSidebar 
+                    property={property}
+                    userId={userId}
+                />
+            </div>
+        </main>
     )
-    
 }
 
 export default PropertyDetailPage;
